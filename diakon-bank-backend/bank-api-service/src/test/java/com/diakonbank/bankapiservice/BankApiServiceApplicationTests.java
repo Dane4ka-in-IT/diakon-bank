0b705c91b619762f6116ced092f1a130500e985f
ITdiakon <145962819+Dane4ka-in-IT@users.noreply.github.com>
2025-10-31 23:53:17 +0300

    init multi-module maven structere and add docker-compose for PsSQL

---
+++ diakon-bank-backend/bank-api-service/src/test/java/com/diakonbank/bankapiservice/BankApiServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.diakonbank.bankapiservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankApiServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
